4196c1de81cba294c81255ff73189cb69d575e09
Swaggypanties <kausejere@gmail.com>
2024-10-22 01:49:22 +0300

    Removed analytics

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -18,7 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 
 export async function loginUser(username: string, password: string) {
     const email = `${username}@codedamn.com`
