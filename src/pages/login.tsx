d0bcefa88e5b10dd01abad025e3b15687b718c1a
MarkusLaine19 <127027582+MarkusLaine19@users.noreply.github.com>
2024-10-22 20:31:48 +0300

    pretty much done

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,23 @@
 import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput } from '@ionic/react';
 import React, { useState } from 'react';
 import { loginUser } from '../firebaseConfig';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom'; // Import useHistory
 import { toast } from '../toast';
 
-
 const Login: React.FC = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory(); // Initialize useHistory
 
     async function login() {
-       const res = await loginUser(username, password)
+       const res = await loginUser(username, password);
        if (!res) {
-            toast('Error logging with your credentials')
+           toast('Error logging in with your credentials');
        } else {
-        toast('You have logged in!')
+           toast('You have logged in!');
+           history.push('/moviepage'); // Redirect to moviepage after successful login
        }
     }
-
 
     return (
         <IonPage>
@@ -27,15 +27,16 @@ const Login: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent className="ion-padding">
-                <IonInput placeholder="Username" 
-                onIonChange={(e: any) => setUsername(e.target.value)}/>
-                <IonInput 
-                type='password'
-                placeholder="Password"
-                onIonChange={(e: any) => setPassword(e.target.value)}/>
-
+                <IonInput
+                    placeholder="Username"
+                    onIonChange={(e: any) => setUsername(e.target.value)}
+                />
+                <IonInput
+                    type="password"
+                    placeholder="Password"
+                    onIonChange={(e: any) => setPassword(e.target.value)}
+                />
                 <IonButton onClick={login}>Login</IonButton>
-
                 <p> Don't have an account yet? <Link to="/register">Register</Link> </p>
             </IonContent>
         </IonPage>
